4c7814a5665df351a4c7c2c3588b564afa241fb8
wander23 <57674214+wander23@users.noreply.github.com>
2021-11-10 17:22:08 +0700

    Update Register
    Có thể đăng ký tài khoản được rồi

--- source/src/components/Login/Register.js
+++ source/src/components/Login/Register.js
@@ -1,10 +1,23 @@
 // import logo from './logo.svg';
 import '../../css/login_style.css'
-import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Switch_log from './Switch_log';
-// import signup from '../../authentication/startup'
+import {signup} from '../authentication/startup'
+import {useRef, useState} from 'react';
 
 function Register() {
+    const[Waiting, setWaiting] = useState(false);
+    const floatingInputEmail = useRef()
+    const floatingPassword = useRef()
+    const floatingPasswordConfirm = useRef()
+
+    async function handSignUp()
+    {
+      setWaiting(true);
+      await signup(floatingInputEmail.current.value,floatingPassword.current.value,floatingPasswordConfirm.current.value)
+      setWaiting(false);
+    }
+
     return (
         <div className="card-body">
                 <h5 className="card-title text-center mb-5 font-weight-bold fs-1">Register</h5>
@@ -13,7 +26,7 @@ function Register() {
                   <div className="input-group mb-3 rounded-3">
                     <span className="icon fas fa-envelope" />
                     <div className="form-floating">
-                      <input type="email" className="form-control" id="floatingInputEmail" placeholder="name@example.com" />
+                      <input type="email" className="form-control" ref={floatingInputEmail} placeholder="name@example.com" />
                       <label htmlFor="floatingInputEmail">Email address</label>
                     </div>
                   </div>
@@ -21,19 +34,19 @@ function Register() {
                   <div className="input-group rounded-top">
                     <span className="icon fas fa-key" />
                     <div className="form-floating ">
-                      <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                      <input type="password" className="form-control" ref={floatingPassword} placeholder="Password" />
                       <label htmlFor="floatingPassword">Password</label>
                     </div>
                   </div>
                   <div className="input-group mb-4 rounded-bottom">
                     <div className="icon" />
                     <div className="form-floating hi">
-                      <input type="password" className="form-control hi" id="floatingPasswordConfirm" placeholder="Confirm Password" />
+                      <input type="password" className="form-control hi" ref={floatingPasswordConfirm} placeholder="Confirm Password" />
                       <label htmlFor="floatingPasswordConfirm">Confirm Password</label>
                     </div>
                   </div>
                   <div className=" text-center d-grid mb-2">
-                    <button className="btn btn-lg btn-primary btn-login fw-bold text-uppercase" type="submit" id = 'signupBtn'>Register</button>
+                    <button className="btn btn-lg btn-primary btn-login fw-bold text-uppercase" disabled={Waiting} type="submit" onClick={handSignUp}>Register</button>
                   </div>
                   <hr className="my-4" />
                   <div className="text-center d-grid mb-2">
