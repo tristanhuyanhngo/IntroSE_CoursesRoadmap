8e40bc8e10830744f35a3bc6403f58114d045735
QuanBlue <quannguyenthanh558@gmail.com>
2021-11-29 20:30:03 +0000

    convert User.js to Function Programming

--- source/src/components/Admin_page/User.js
+++ source/src/components/Admin_page/User.js
@@ -1,52 +1,47 @@
 import './user.css'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../../node_modules/bootstrap/dist/js/bootstrap'
-import React, { Component }  from 'react';
+import React, { useState, Component }  from 'react';
 import { BsBackspaceFill,BsFillGearFill } from '../../../node_modules/react-icons/bs';
 //import { auth } from '../../Handler/filebase_config';
 
 
-class User extends Component { 
-    constructor(props){
-        super(props);
-        this.state = {
-            props,
-            key: props.id,
-            index: props.index + 1,
-            id: props.id,
-            avatar: props.avatar,
-            name: props.name,
-            create: props.create,
-            role: props.role,
-            status: props.status,
-            open: false
-        }
-    }
+export default function User(props){
+    const [key, setKey] = useState(props.id);
+    const [index, setIndex] = useState(props.index + 1);
+    const [id, setId] = useState(props.id);
+    const [avatar, setAvatar] = useState(props.avatar);
+    const [name, setName] = useState(props.name);
+    const [create, setCreate] = useState(props.create);
+    const [role, setRole] = useState(props.role);
+    const [status, setStatus] = useState(props.status);
+    const [open, setOpen] = useState(false);
 
-    status_dot = (status) => {
+    function status_dot() {
         if (status === 'active')
             return <div className="col-1 status-active">&#x25cf;</div>  
         else
             return <div className="col-1 status-inactive">&#x25cf;</div> 
         }
-
-    click_setting = () => {
-        console.log('click_setting:', this.open)
-        if (this.state.open == false)
-            this.setState({open:true})
+    
+    function click_setting (){
+        console.log('click_setting:', open)
+        if (open == false)
+            setOpen(true)
         else
-            this.setState({open:false})
+            setOpen(false)
         
     }
-
-    render_set_role =() =>{
-        var id_select = 'select' + this.state.id
-        console.log('click:', id_select,'- open:',this.state.open)
-
-        if (this.state.open == true)
+    
+    function render_set_role (){
+        // console.log('state:',state)
+        var id_select = 'select' + id;
+        console.log('click:', id_select,'- open:',open)
+    
+        if (open == true)
             return (
-                <div className="row mt-2 pe-3" id={this.state.id}>
-                    <select className="form-select" id={id_select} onChange={()=>this.set_role(id_select)}>
+                <div className="row mt-2 pe-3" id={id}>
+                    <select className="form-select" id={id_select} onChange={()=>set_role(id_select)}>
                         <option value="Admin"> Admin</option>
                         <option value="Sensor"> Sensor</option>
                         <option value="Poster"> Poster</option>
@@ -55,57 +50,52 @@ class User extends Component {
                 </div>
             );
     }
-
-    set_role = (id_select) =>{
+    
+    function set_role(id_select){
         let sl = document.getElementById(id_select)
         let role = sl.options[sl.selectedIndex].value
-        this.setState({role:role})
+        setRole(role)
         console.log('set role:', role)
     }
 
-    render() {
-        var id1 = '#' + this.state.id
-        
-        console.log(id1)
+    
 
-        return (
-        <tr>
-            <td  scope='row'>{this.state.index}</td>
-            <td className = 'col'>
+    return (
+    <tr>
+        <td  scope='row'>{index}</td>
+        <td className = 'col'>
+            <div className='row'>
+                <div className='col-2 d-flex align-items-center justify-content-center'>
+                    <img src={avatar} className="avatar" alt="Avatar" />
+                </div>
+                <strong className='col'> {name}</strong>
+            </div>
+        </td>
+        <td>{create}</td>                        
+        <td>{role}</td>
+        <td className='col'>
+            <div className='row d-flex align-items-center justify-content-center'>
+                { status_dot()}
+                <div className='col'>{status}</div>
+            </div>
+        </td>
+        <td className='col'>
+            <div className='col'>
                 <div className='row'>
-                    <div className='col-2 d-flex align-items-center justify-content-center'>
-                        <img src={this.state.avatar} className="avatar" alt="Avatar" />
-                    </div>
-                    <strong className='col'> {this.state.name}</strong>
+                    <button type="button" className="col-5 btn btn-secondary me-1 d-flex align-items-center justify-content-center" 
+                    onClick={()=>click_setting()}>
+                        <BsFillGearFill/>
+                    </button>
+                    
+                    <button type="button" className="col-5 btn btn-secondary delete d-flex align-items-center justify-content-center" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete">
+                        <BsBackspaceFill/>
+                    </button>
                 </div>
-            </td>
-            <td>{this.state.create}</td>                        
-            <td>{this.state.role}</td>
-            <td className='col'>
-                <div className='row d-flex align-items-center justify-content-center'>
-                    { this.status_dot(this.state.status)}
-                    <div className='col'>{this.state.status}</div>
-                </div>
-            </td>
-            <td className='col'>
-                <div className='col'>
-                    <div className='row'>
-                        <button type="button" className="col-5 btn btn-secondary me-1 d-flex align-items-center justify-content-center" 
-                        onClick={()=>this.click_setting()}>
-                            <BsFillGearFill/>
-                        </button>
-                        
-                        <button type="button" className="col-5 btn btn-secondary delete d-flex align-items-center justify-content-center" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete">
-                            <BsBackspaceFill/>
-                        </button>
-                    </div>
-                
-                    {this.render_set_role()}
-                </div>
-                
-            </td>
-        </tr>
-        );
-    }
+            
+                {render_set_role()}
+            </div>
+            
+        </td>
+    </tr>
+    );
 }
-export default User;
